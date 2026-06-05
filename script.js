bd011506a138e34d6b9ae224d0b043ab0a160d23
Denner Moore <dennerrodriguesx@gmail.com>
2026-06-05 18:28:45 -0400

    Refactor script.js to include Supabase key

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const SUPABASE_URL =
 "https://ntxfyzdumgozwtyufoor.supabase.co";
 
 const SUPABASE_KEY =
-"SUA_CHAVE_AQUI";
+"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im50eGZ5emR1bWdvend0eXVmb29yIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA2NjczNDEsImV4cCI6MjA5NjI0MzM0MX0.nL0kXMEsAdq9RC9QerUsf7xSnWUNegkYp4PGPkzQrKE";
 
 const banco =
 window.supabase.createClient(
